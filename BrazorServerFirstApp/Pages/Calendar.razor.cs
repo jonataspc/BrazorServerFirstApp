b309a5d2d4d05c7486194fd013de172266e1457b
Jonatas Cruz <jonataspc@gmail.com>
2021-09-15 02:16:11 -0300

    feat: adding remove appointment button on edit dialog.

--- BrazorServerFirstApp/Pages/Calendar.razor.cs
+++ BrazorServerFirstApp/Pages/Calendar.razor.cs
@@ -1,10 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using BrazorServerFirstApp.Models;
+﻿using BrazorServerFirstApp.Models;
 using Radzen;
 using Radzen.Blazor;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace BrazorServerFirstApp.Pages
 {
@@ -19,11 +18,11 @@ namespace BrazorServerFirstApp.Pages
         }
 
 
-         RadzenScheduler<Appointment> scheduler;
-    //EventConsole console;
-    Dictionary<DateTime, string> events = new Dictionary<DateTime, string>();
+        RadzenScheduler<Appointment> scheduler;
+        //EventConsole console;
+        Dictionary<DateTime, string> events = new Dictionary<DateTime, string>();
 
-    IList<Appointment> appointments = new List<Appointment>
+        IList<Appointment> appointments = new List<Appointment>
     {
     new Appointment { Start = DateTime.Today.AddDays(-2), End = DateTime.Today.AddDays(-2), Text = "Birthday" },
     new Appointment { Start = DateTime.Today.AddDays(-11), End = DateTime.Today.AddDays(-10), Text = "Day off" },
@@ -34,38 +33,43 @@ namespace BrazorServerFirstApp.Pages
     new Appointment { Start = DateTime.Today.AddDays(1), End = DateTime.Today.AddDays(12), Text = "Vacation" },
     };
 
-    async Task OnSlotSelect(SchedulerSlotSelectEventArgs args)
-    {
-        //console.Log($"SlotSelect: Start={args.Start} End={args.End}");
-
-        Appointment data = await DialogService.OpenAsync<AddAppointmentPage>("Add Appointment",
-            new Dictionary<string, object> { { "Start", args.Start }, { "End", args.End } });
-
-        if (data != null)
+        async Task OnSlotSelect(SchedulerSlotSelectEventArgs args)
         {
-            appointments.Add(data);
-            // Either call the Reload method or reassign the Data property of the Scheduler
+            //console.Log($"SlotSelect: Start={args.Start} End={args.End}");
+
+            Appointment data = await DialogService.OpenAsync<AddAppointmentPage>("Add Appointment",
+                new Dictionary<string, object> { { "Start", args.Start }, { "End", args.End } });
+
+            if (data != null)
+            {
+                appointments.Add(data);
+                // Either call the Reload method or reassign the Data property of the Scheduler
+                await scheduler.Reload();
+            }
+        }
+
+        public async Task OnAppointmentSelect(SchedulerAppointmentSelectEventArgs<Appointment> args)
+        {
+            //console.Log($"AppointmentSelect: Appointment={args.Data.Text}");
+
+            Appointment data = await DialogService.OpenAsync<EditAppointmentPage>("Edit Appointment", new Dictionary<string, object> { { "Appointment", args.Data } });
+
+            if (data != null)
+            {
+                appointments.Remove(data);
+            }
+
             await scheduler.Reload();
         }
-    }
 
-    public async Task OnAppointmentSelect(SchedulerAppointmentSelectEventArgs<Appointment> args)
-    {
-        //console.Log($"AppointmentSelect: Appointment={args.Data.Text}");
-
-        await DialogService.OpenAsync<EditAppointmentPage>("Edit Appointment", new Dictionary<string, object> { { "Appointment", args.Data } });
-
-        await scheduler.Reload();
-    }
-
-    public void OnAppointmentRender(SchedulerAppointmentRenderEventArgs<Appointment> args)
-    {
-        // Never call StateHasChanged in AppointmentRender - would lead to infinite loop
-
-        if (args.Data.Text == "Birthday")
+        public void OnAppointmentRender(SchedulerAppointmentRenderEventArgs<Appointment> args)
         {
-            args.Attributes["style"] = "background: red";
+            // Never call StateHasChanged in AppointmentRender - would lead to infinite loop
+
+            if (args.Data.Text == "Birthday")
+            {
+                args.Attributes["style"] = "background: red";
+            }
         }
-    }
     }
 }
